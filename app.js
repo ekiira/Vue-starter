f135bb0ba1c2b5aa720c2395ed159ee2a0e3d679
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2021-10-13 19:48:46 +0100

    computed properties

--- app.js
+++ app.js
@@ -3,19 +3,25 @@ const app = Vue.createApp({
     return {
       showBooks: true,
       books: [{
+          id: 1,
           title: "The Final Empire",
           author: "Yaw Baldwin",
-          image: 'assets/avatar.jpeg'
+          image: 'assets/avatar.jpeg',
+          isFav: true
         },
         {
+          id: 2,
           title: "The Final Empire II",
           author: "Yaw Baldwin",
-          image: 'assets/avatar.jpeg'
+          image: 'assets/avatar.jpeg',
+          isFav: false
         },
         {
+          id: 3,
           title: "Dumbledore",
           author: "J.K. Rowling",
-          image: 'assets/avatar1.jpeg'
+          image: 'assets/avatar1.jpeg',
+          isFav: true
         }
       ],
     };
@@ -24,7 +30,20 @@ const app = Vue.createApp({
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
     },
+    toggleFav(book) {
+      book.isFav = !book.isFav
+      // this.books?.filter((book) => {
+      //   if (book.id === id) {
+      //     book.isFav = !book.isFav
+      //   }
+      // })
+    }
   },
+  computed: {
+    filteredBooks() {
+     return this.books?.filter((book) => book.isFav)
+    }
+  }
 });
 
 app.mount("#app");
